856640f2c3e0df6fc1a8e46124cdc49f9225a30a
chrisna <chrisna.mitt@gmail.com>
2024-11-20 19:23:10 +0100

    test1

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
-alert('fever')
+const boutonPoke = document.querySelector('.boutonPoke')
+const colonne =document.querySelector('.colonne')
+
+
+
+
+console.log('test1')
+
+function faitUneCartePoke() {
+    fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+        .then(res => res.json)
+        .then(data => ` 
+  <div class="card" style="width: 18rem;">
+  <img src="images/test.png" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>` )
+
+
+}
+console.log('test2')
+
+
+boutonPoke.addEventListener('click', faitUneCartePoke);
+
