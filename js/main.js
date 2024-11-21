84a139fb73177d3daf38c6e29b5e16fa78f9cdd8
chrisna <chrisna.mitt@gmail.com>
2024-11-21 18:38:35 +0100

    ça n'affiche pas les cartes

--- js/main.js
+++ js/main.js
@@ -2,27 +2,33 @@ const boutonPoke = document.querySelector('.boutonPoke')
 const colonne =document.querySelector('.colonne')
 
 
-
-
+boutonPoke.addEventListener('click', faitUneCartePoke);
 console.log('test1')
 
 function faitUneCartePoke() {
-    fetch('https://pokeapi.co/api/v2/pokemon/ditto')
-        .then(res => res.json)
-        .then(data => ` 
-  <div class="card" style="width: 18rem;">
-  <img src="images/test.png" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>` )
+    fetch('https://pokebuildapi.fr/api/v1/pokemon')
+        .then(res => res.json())
+        .then(data => {
 
+            const randomPoke = data[Math.floor(Math.random() * data.length)]; // permet de choisir aléatoirement un pokemon
+
+            const cardPoke =
+                `
+            <div class="card" style="width: 18rem; height: 480px" >
+              <img src="${randomPoke.image}" class="card-img-top" alt=""> 
+              <div class="card-body">
+                <h5 class="card-title">${randomPoke.name}</h5>
+                <p class="card-text"></p>
+              </div>
+            </div>
+
+                `;
+            colonne.innerHTML += faitUneCartePoke(); // permet de mettre à la suite les cartes quand j'appuie sur le bouton
+        })
 
 }
 console.log('test2')
 
 
-boutonPoke.addEventListener('click', faitUneCartePoke);
+
 
